b79d7f7fbd025651aefe611ecca723e5c5c312d9
Meen Philip <meenphilip@gmail.com>
2023-01-06 22:42:57 +0300

    Display balance, income & expense.

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const balance = document.getElementById('balance');
-const income = document.getElementById('money-plus');
-const expense = document.getElementById('money-minus');
+const incomeEl = document.getElementById('money-plus');
+const expenseEl = document.getElementById('money-minus');
 const list = document.getElementById('list');
 const form = document.getElementById('form');
 const text = document.getElementById('text');
@@ -37,11 +37,33 @@ function addTransactionDOM(transaction) {
   list.appendChild(listItem);
 }
 
+// Update the Balance, Income,& Expense
+function upadateValues() {
+  const amounts = transactions.map(transaction => transaction.amount);
+  const total = amounts.reduce((acc, item) => acc + item, 0).toFixed(2);
+
+  const income = amounts
+    .filter(item => item > 0)
+    .reduce((acc, item) => (acc += item), 0)
+    .toFixed(2);
+
+  const expense = (
+    amounts.filter(item => item < 0).reduce((acc, item) => (acc += item), 0) *
+    -1
+  ).toFixed(2);
+
+  // Insert into DOM
+  balance.innerText = `$${total}`;
+  incomeEl.innerText = `$${income}`;
+  expenseEl.innerText = `$${expense}`;
+}
+
 // Init app
 function init() {
   list.innerHTML = '';
   // transactions.forEach(addTransactionDOM);
   transactions.map(addTransactionDOM);
+  upadateValues();
 }
 
 init();
